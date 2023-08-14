111e5421cffed9268ff6af8a25fcaa990c779503
Benoit Marcot <benoit@libertify.com>
2023-08-14 15:08:23 +0200

    Add plugin support for protocol-v2 with custom proxy

--- src/handle_query_contract_id.c
+++ src/handle_query_contract_id.c
@@ -1,25 +1,23 @@
 #include "libertify_plugin.h"
 
+// Sets the first screen to display.
 void handle_query_contract_id(void *parameters) {
     ethQueryContractID_t *msg = (ethQueryContractID_t *) parameters;
-    one_inch_parameters_t *context = (one_inch_parameters_t *) msg->pluginContext;
+    const context_t *context = (const context_t *) msg->pluginContext;
+    // msg->name will be the upper sentence displayed on the screen.
+    // msg->version will be the lower sentence displayed on the screen.
 
+    // For the first screen, display the plugin name.
     strlcpy(msg->name, PLUGIN_NAME, msg->nameLength);
 
-    switch (context->selectorIndex) {
-        case DEPOSIT:
-        case DEPOSIT_ETH:
-            strlcpy(msg->version, "Deposit", msg->versionLength);
-            break;
-        case REDEEM:
-        case REDEEM_ETH:
-            strlcpy(msg->version, "Withdraw", msg->versionLength);
-            break;
-        default:
-            PRINTF("Selector Index :%d not supported\n", context->selectorIndex);
-            msg->result = ETH_PLUGIN_RESULT_ERROR;
-            return;
+    if ((context->selectorIndex == DEPOSIT) || (context->selectorIndex == DEPOSIT_ETH)) {
+        strlcpy(msg->version, "Deposit", msg->versionLength);
+        msg->result = ETH_PLUGIN_RESULT_OK;
+    } else if ((context->selectorIndex == WITHDRAW) || (context->selectorIndex == WITHDRAW_ETH)) {
+        strlcpy(msg->version, "Withdraw", msg->versionLength);
+        msg->result = ETH_PLUGIN_RESULT_OK;
+    } else {
+        PRINTF("Selector index: %d not supported\n", context->selectorIndex);
+        msg->result = ETH_PLUGIN_RESULT_ERROR;
     }
-
-    msg->result = ETH_PLUGIN_RESULT_OK;
 }
